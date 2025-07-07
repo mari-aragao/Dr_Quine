14ede60842100abe00b5945897c4137cea910f84
mari-aragao <mariazevedoaragao@gmail.com>
2025-07-06 23:08:56 -0300

    style: align comments with the required format

--- C/Colleen.c
+++ C/Colleen.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 
-//comment outside the program
+/*
+    This program will print its own source when run.
+*/
 
 void another_function() {}
 
 int main() {
-    //comment in the main
-    char *quine = "#include <stdio.h>%c%c//comment outside the program%c%cvoid another_function() {}%c%cint main() {%c    //comment in the main%c    char *quine = %c%s%c;%c    printf(quine, 10, 10, 10, 10, 10, 10, 10, 10, 34, quine, 34, 10, 10, 10, 10, 10);%c    another_function();%c    return 0;%c}%c";
-    printf(quine, 10, 10, 10, 10, 10, 10, 10, 10, 34, quine, 34, 10, 10, 10, 10, 10);
+/*
+    This program will print its own source when run.
+*/
+    char *quine = "#include <stdio.h>%c%c/*%c    This program will print its own source when run.%c*/%c%cvoid another_function() {}%c%cint main() {%c/*%c    This program will print its own source when run.%c*/%c    char *quine = %c%s%c;%c    printf(quine, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 34, quine, 34, 10, 10, 10, 10, 10);%c    another_function();%c    return 0;%c}%c";
+    printf(quine, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 34, quine, 34, 10, 10, 10, 10, 10);
     another_function();
     return 0;
 }
--- C/Grace.c
+++ C/Grace.c
@@ -2,10 +2,12 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-//comment required
+/*
+    This program will print its own source when run.
+*/
 
 #define FILE_NAME "Grace_kid.c"
-#define QUINE "#include <stdio.h>%c#include <fcntl.h>%c#include <unistd.h>%c%c//comment required%c%c#define FILE_NAME %cGrace_kid.c%c%c#define QUINE %c%s%c%c#define GRACE() int main() {int fd = open(FILE_NAME, O_WRONLY | O_CREAT | O_TRUNC, 0644); if (fd < 0) return -1; dprintf(fd, QUINE, 10, 10, 10, 10, 10, 10, 34, 34, 10, 34, QUINE, 34, 10, 10, 10, 10); close(fd); return 0;}%c%cGRACE()%c"
-#define GRACE() int main() {int fd = open(FILE_NAME, O_WRONLY | O_CREAT | O_TRUNC, 0644); if (fd < 0) return -1; dprintf(fd, QUINE, 10, 10, 10, 10, 10, 10, 34, 34, 10, 34, QUINE, 34, 10, 10, 10, 10); close(fd); return 0;}
+#define QUINE "#include <stdio.h>%c#include <fcntl.h>%c#include <unistd.h>%c%c/*%c    This program will print its own source when run.%c*/%c%c#define FILE_NAME %cGrace_kid.c%c%c#define QUINE %c%s%c%c#define GRACE() int main() {int fd = open(FILE_NAME, O_WRONLY | O_CREAT | O_TRUNC, 0644); if (fd < 0) return -1; dprintf(fd, QUINE, 10, 10, 10, 10, 10, 10, 10, 10, 34, 34, 10, 34, QUINE, 34, 10, 10, 10, 10); close(fd); return 0;}%c%cGRACE()%c"
+#define GRACE() int main() {int fd = open(FILE_NAME, O_WRONLY | O_CREAT | O_TRUNC, 0644); if (fd < 0) return -1; dprintf(fd, QUINE, 10, 10, 10, 10, 10, 10, 10, 10, 34, 34, 10, 34, QUINE, 34, 10, 10, 10, 10); close(fd); return 0;}
 
 GRACE()
