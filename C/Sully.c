1bf7442d60b368fe344ec3cedec11158cec525ac
mari-aragao <mariazevedoaragao@gmail.com>
2025-07-08 17:13:30 -0300

    fix: adjust decrement logic

--- C/Sully.c
+++ C/Sully.c
@@ -10,7 +10,7 @@ int main() {
     char compile_cmd[128];
     char execute_cmd[64];
 
-    if (i < 0) return 0;
+    if (--i < 0) return 0;
 
     snprintf(filename, sizeof(filename), "Sully_%d.c", i);
     fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
@@ -20,8 +20,8 @@ int main() {
         return -1;
     }
     
-    char *code = "#include <stdio.h>%c#include <fcntl.h>%c#include <unistd.h>%c#include <stdlib.h>%c%cint main() {%c    int i = %d;%c    int fd;%c    char filename[64];%c    char compile_cmd[128];%c    char execute_cmd[64];%c%c    if (i < 0) return 0;%c%c    snprintf(filename, sizeof(filename), %cSully_%%d.c%c, i);%c    fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);%c    if (fd < 0)%c    {%c        perror(%cError in open%c);%c        return -1;%c    }%c    %c    char *code = %c%s%c;%c    dprintf(fd, code, 10, 10, 10, 10, 10, 10, i - 1, 10, 10, 10, 10, 10, 10, 10, 10, 34, 34, 10, 10, 10, 10, 34, 34, 10, 10, 10, 10, 34, code, 34, 10, 10, 10, 10, 10, 10, 34, 34, 10, 10, 34, 34, 10, 10, 10, 10, 34, 34, 10, 10, 34, 34, 10, 10, 10, 10, 10);%c%c    close(fd);%c    if (fd < 0) return -1;%c%c    snprintf(compile_cmd, sizeof(compile_cmd), %cgcc -Wall -Wextra %%s -o Sully_%%d%c, filename, i);%c    if (system(compile_cmd) != 0) {%c        fprintf(stderr, %cCompilation failed%c);%c        return -1;%c    }%c%c    snprintf(execute_cmd, sizeof(execute_cmd), %c./Sully_%%d%c, i);%c    if (system(execute_cmd) != 0) {%c        fprintf(stderr, %cExecution failed%c);%c        return -1;%c    }%c%c    return 0;%c}";
-    dprintf(fd, code, 10, 10, 10, 10, 10, 10, i - 1, 10, 10, 10, 10, 10, 10, 10, 10, 34, 34, 10, 10, 10, 10, 34, 34, 10, 10, 10, 10, 34, code, 34, 10, 10, 10, 10, 10, 10, 34, 34, 10, 10, 34, 34, 10, 10, 10, 10, 34, 34, 10, 10, 34, 34, 10, 10, 10, 10, 10);
+    char *code = "#include <stdio.h>%c#include <fcntl.h>%c#include <unistd.h>%c#include <stdlib.h>%c%cint main() {%c    int i = %d;%c    int fd;%c    char filename[64];%c    char compile_cmd[128];%c    char execute_cmd[64];%c%c    if (--i < 0) return 0;%c%c    snprintf(filename, sizeof(filename), %cSully_%%d.c%c, i);%c    fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);%c    if (fd < 0)%c    {%c        perror(%cError in open%c);%c        return -1;%c    }%c    %c    char *code = %c%s%c;%c    dprintf(fd, code, 10, 10, 10, 10, 10, 10, i, 10, 10, 10, 10, 10, 10, 10, 10, 34, 34, 10, 10, 10, 10, 34, 34, 10, 10, 10, 10, 34, code, 34, 10, 10, 10, 10, 10, 10, 34, 34, 10, 10, 34, 34, 10, 10, 10, 10, 34, 34, 10, 10, 34, 34, 10, 10, 10, 10, 10);%c%c    close(fd);%c    if (fd < 0) return -1;%c%c    snprintf(compile_cmd, sizeof(compile_cmd), %cgcc -Wall -Wextra %%s -o Sully_%%d%c, filename, i);%c    if (system(compile_cmd) != 0) {%c        fprintf(stderr, %cCompilation failed%c);%c        return -1;%c    }%c%c    snprintf(execute_cmd, sizeof(execute_cmd), %c./Sully_%%d%c, i);%c    if (system(execute_cmd) != 0) {%c        fprintf(stderr, %cExecution failed%c);%c        return -1;%c    }%c%c    return 0;%c}";
+    dprintf(fd, code, 10, 10, 10, 10, 10, 10, i, 10, 10, 10, 10, 10, 10, 10, 10, 34, 34, 10, 10, 10, 10, 34, 34, 10, 10, 10, 10, 34, code, 34, 10, 10, 10, 10, 10, 10, 34, 34, 10, 10, 34, 34, 10, 10, 10, 10, 34, 34, 10, 10, 34, 34, 10, 10, 10, 10, 10);
 
     close(fd);
     if (fd < 0) return -1;
