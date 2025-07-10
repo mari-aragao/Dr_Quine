097b36f841013e23d8459fbe47324d4778e87929
mari-aragao <mariazevedoaragao@gmail.com>
2025-07-09 23:14:22 -0300

    refactor: remove extra format arg in grace.s

--- ASM/Grace.s
+++ ASM/Grace.s
@@ -2,7 +2,7 @@
 global main
 extern dprintf
 
-%define QUINE "; comment outside the program%1$cglobal main%1$cextern dprintf%1$c%1$c%3$cdefine QUINE %2$c%4$s%2$c%1$c%3$cdefine FILENAME %2$cGrace_kid.s%2$c%1$c%3$cdefine MAIN main:%1$c%1$c%3$cmacro GRACE 0%1$cMAIN%1$c    push rbp%1$c    mov rbp, rsp%1$c%1$copen:%1$c    mov rax, 2%1$c    lea rdi, [rel filename]%1$c    mov rsi, 0x241%1$c    mov rdx, 0644%1$c    syscall%1$c    mov rbx, rax%1$c    cmp rbx, 0%1$c    jb end%1$c%1$cwrite:%1$c    mov rdi, rbx%1$c    lea rsi, [rel quine]%1$c    mov rdx, 10%1$c    mov rcx, 34%1$c    mov r8, 37%1$c    lea r9, [rel quine]%1$c    call dprintf wrt ..plt%1$c%1$cclose:%1$c    mov rax, 3%1$c    mov rdi, rbx%1$c    syscall%1$c%1$cend:%1$c    leave%1$c    ret%1$c%1$c%3$cendmacro%1$c%1$csection .data%1$c    quine db QUINE, 0%1$c    filename db FILENAME, 0%1$c%1$csection .text%1$cGRACE"
+%define QUINE "; comment outside the program%1$cglobal main%1$cextern dprintf%1$c%1$c%%define QUINE %2$c%3$s%2$c%1$c%%define FILENAME %2$cGrace_kid.s%2$c%1$c%%define MAIN main:%1$c%1$c%%macro GRACE 0%1$cMAIN%1$c    push rbp%1$c    mov rbp, rsp%1$c%1$copen:%1$c    mov rax, 2%1$c    lea rdi, [rel filename]%1$c    mov rsi, 0x241%1$c    mov rdx, 0644%1$c    syscall%1$c    mov rbx, rax%1$c    cmp rbx, 0%1$c    jl end%1$c%1$cwrite:%1$c    mov rdi, rbx%1$c    lea rsi, [rel quine]%1$c    mov rdx, 10%1$c    mov rcx, 34%1$c    lea r8, [rel quine]%1$c    call dprintf wrt ..plt%1$c%1$cclose:%1$c    mov rax, 3%1$c    mov rdi, rbx%1$c    syscall%1$c%1$cend:%1$c    leave%1$c    ret%1$c%1$c%%endmacro%1$c%1$csection .data%1$c    quine db QUINE, 0%1$c    filename db FILENAME, 0%1$c%1$csection .text%1$cGRACE"
 %define FILENAME "Grace_kid.s"
 %define MAIN main:
 
@@ -19,15 +19,14 @@ open:
     syscall
     mov rbx, rax
     cmp rbx, 0
-    jb end
+    jl end
 
 write:
     mov rdi, rbx
     lea rsi, [rel quine]
     mov rdx, 10
     mov rcx, 34
-    mov r8, 37
-    lea r9, [rel quine]
+    lea r8, [rel quine]
     call dprintf wrt ..plt
 
 close:
