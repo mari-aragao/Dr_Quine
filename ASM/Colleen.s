758d36eb78793301a0364aef8c748aeade10ef69
maragao <maragao@student.42.rio>
2025-08-19 18:38:37 -0300

    fix: clear the stack before the printf call in Colleen.s

--- ASM/Colleen.s
+++ ASM/Colleen.s
@@ -3,7 +3,7 @@ global main
 extern printf
 
 section .data
-    quine db ";comment outside the program%1$cglobal main%1$cextern printf%1$c%1$csection .data%1$c    quine db %2$c%3$s%2$c, 0%1$c%1$csection .text%1$canother_function:%1$c    ret%1$c%1$cmain:%1$c    ; comment inside the main%1$c    lea rdi, [rel quine]%1$c    mov rsi, 10%1$c    mov rdx, 34%1$c    lea rcx, [rel quine]%1$c    xor rax, rax%1$c    call printf wrt ..plt%1$c    call another_function%1$c    ret%1$c", 0
+    quine db ";comment outside the program%1$cglobal main%1$cextern printf%1$c%1$csection .data%1$c    quine db %2$c%3$s%2$c, 0%1$c%1$csection .text%1$canother_function:%1$c    ret%1$c%1$cmain:%1$c    ; comment inside the main%1$c    push rbp%1$c    mov rbp, rsp%1$c%1$c    lea rdi, [rel quine]%1$c    mov rsi, 10%1$c    mov rdx, 34%1$c    lea rcx, [rel quine]%1$c    xor rax, rax%1$c    call printf wrt ..plt%1$c    call another_function%1$c    leave%1$c    ret%1$c", 0
 
 section .text
 another_function:
@@ -11,6 +11,9 @@ another_function:
 
 main:
     ; comment inside the main
+    push rbp
+    mov rbp, rsp
+
     lea rdi, [rel quine]
     mov rsi, 10
     mov rdx, 34
@@ -18,4 +21,5 @@ main:
     xor rax, rax
     call printf wrt ..plt
     call another_function
+    leave
     ret
