3267cb2065be3a27846b0302257f40ca07862487
mari-aragao <mariazevedoaragao@gmail.com>
2025-08-24 20:39:41 -0300

    fix: remove extra define in Grace.s

--- ASM/Grace.s
+++ ASM/Grace.s
@@ -2,12 +2,10 @@
 global main
 extern dprintf
 
-%define QUINE "; comment outside the program%1$cglobal main%1$cextern dprintf%1$c%1$c%%define QUINE %2$c%3$s%2$c%1$c%%define FILENAME %2$cGrace_kid.s%2$c%1$c%%define MAIN main:%1$c%1$c%%macro GRACE 0%1$cMAIN%1$c    push rbp%1$c    mov rbp, rsp%1$c%1$copen:%1$c    mov rax, 2%1$c    lea rdi, [rel filename]%1$c    mov rsi, 0x241%1$c    mov rdx, 420%1$c    syscall%1$c    mov rbx, rax%1$c    cmp rbx, 0%1$c    jl end%1$c%1$cwrite:%1$c    mov rdi, rbx%1$c    lea rsi, [rel quine]%1$c    mov rdx, 10%1$c    mov rcx, 34%1$c    lea r8, [rel quine]%1$c    call dprintf wrt ..plt%1$c%1$cclose:%1$c    mov rax, 3%1$c    mov rdi, rbx%1$c    syscall%1$c%1$cend:%1$c    leave%1$c    ret%1$c%1$c%%endmacro%1$c%1$csection .data%1$c    quine db QUINE, 0%1$c    filename db FILENAME, 0%1$c%1$csection .text%1$cGRACE"
+%define QUINE "; comment outside the program%1$cglobal main%1$cextern dprintf%1$c%1$c%%define QUINE %2$c%3$s%2$c%1$c%%define FILENAME %2$cGrace_kid.s%2$c%1$c%%macro GRACE 0%1$cmain:%1$c    push rbp%1$c    mov rbp, rsp%1$c%1$copen:%1$c    mov rax, 2%1$c    lea rdi, [rel filename]%1$c    mov rsi, 0x241%1$c    mov rdx, 420%1$c    syscall%1$c    mov rbx, rax%1$c    cmp rbx, 0%1$c    jl end%1$c%1$cwrite:%1$c    mov rdi, rbx%1$c    lea rsi, [rel quine]%1$c    mov rdx, 10%1$c    mov rcx, 34%1$c    lea r8, [rel quine]%1$c    call dprintf wrt ..plt%1$c%1$cclose:%1$c    mov rax, 3%1$c    mov rdi, rbx%1$c    syscall%1$c%1$cend:%1$c    leave%1$c    ret%1$c%%endmacro%1$c%1$csection .data%1$c    quine db QUINE, 0%1$c    filename db FILENAME, 0%1$c%1$csection .text%1$cGRACE"
 %define FILENAME "Grace_kid.s"
-%define MAIN main:
-
 %macro GRACE 0
-MAIN
+main:
     push rbp
     mov rbp, rsp
 
@@ -37,7 +35,6 @@ close:
 end:
     leave
     ret
-
 %endmacro
 
 section .data
